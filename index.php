ebba17ca0be488efa1e4aa41401b4cb4307aab26
Aline <alinenaoe@gmail.com>
2019-09-30 12:00:57 -0300

    Estilização do header, header para usuário logado ou não e cards de curso

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $nomeSistema = "Nome da loja";
+    $usuario = ["nome" => "Aline"];
 ?>
 
 <!DOCTYPE html>
@@ -10,29 +11,72 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Loja virtual</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <header class="d-flex justify-content-between align-items-center p-3">
-        <a href="index.php">
+    <header class="navbar bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="img/icon_loja.png" width="30" height="30" alt="">
             <?php echo $nomeSistema; ?>
         </a>
+
         <nav class="justify-content-end">
         <!-- segura o alt e as linhas para escrever ao mesmo tempo -->
             <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cursos</a>    
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>    
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cadastrar</a>    
-                </li>
+
+                <?php
+                    if (isset($usuario) && $usuario != []) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cursos</a>    
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Olá, <?php echo $usuario['nome'];?>!</a>    
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>    
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cadastrar</a>    
+                    </li>
+                <?php } ?>
             </ul>
         </nav>
     </header>
 
+    <main>
+        <section class="container">
+            <div class="card-deck mt-5">
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Nome do curso</h5>
+                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
+                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Nome do curso</h5>
+                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
+                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Nome do curso</h5>
+                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
+                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
+    
      <!-- <?php
         $nome = "Aline";
         $usuario = ["Aline", "Naoe", 30];
