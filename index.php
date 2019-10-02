fc774676ee009ebe519a01f4a2f11996197170c9
Aline <alinenaoe@gmail.com>
2019-10-02 12:02:09 -0300

    cards dinâmicos e menu de categorias dinâmicos usando php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
     $nomeSistema = "Nome da loja";
     $usuario = ["nome" => "Aline"];
+    $produtos = [
+        ["nome" => "Fullstack", "preco" => 3000, "duracao" => "5 meses"],
+        ["nome" => "Marketing Digital", "preco" => 1000, "duracao" => "4 meses"],
+        ["nome" => "Marketing Digital", "preco" => 1000, "duracao" => "4 meses"],
+        ["nome" => "Marketing Digital", "preco" => 1000, "duracao" => "4 meses"]
+    ];
+
+    $categorias = ["Cursos", "Palestras", "Artigos"]
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +18,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Loja virtual</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header class="navbar bg-light">
@@ -45,38 +53,42 @@
         </nav>
     </header>
 
+    <nav id="menu-categorias">
+            <ul class="nav justify-content-center">
+                <?php foreach ($categorias as $categoria) {?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> <?php echo $categoria ?> </a>
+                    </li>
+                <?php } ?>
+            </ul>
+    </nav>
+
     <main>
         <section class="container">
             <div class="card-deck mt-5">
-                <div class="card text-center" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Nome do curso</h5>
-                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
-                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
-                        <a href="#" class="btn btn-primary">Comprar</a>
-                    </div>
-                </div>
-                <div class="card text-center" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Nome do curso</h5>
-                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
-                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
-                        <a href="#" class="btn btn-primary">Comprar</a>
-                    </div>
-                </div>
-                <div class="card text-center" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Nome do curso</h5>
-                        <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
-                        <h5 class="mt-2 mb-2 text-muted">R$ 500</h5>
-                        <a href="#" class="btn btn-primary">Comprar</a>
-                    </div>
-                </div>
+                <?php if(isset($produtos) && $produtos !=[]) {?>
+                    <?php foreach($produtos as $produto) {
+                        //aqui diz que cada item da array produtos é um produto
+                    ?>         
+                        <div class="card text-center" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $produto['nome'] ?></h5>
+                                <img src="img/img_curso_01.jpg" class="card-img-top" alt="...">
+                                <h5 class="mt-2 mb-2 text-muted"><?php echo "R$ ".$produto['preco'] ?></h5>
+                                <a href="#" class="btn btn-primary">Comprar</a>
+                            </div>
+                        </div>
+                    <?php //e aqui fecha a orientação do foreach
+                    } ?>
+                <?php //fechando o if isset 
+                } else {?> 
+                <h3>Não há produtos cadastrados nesta seção</h3>
+                <?php } ?>
             </div>
         </section>
     </main>
 
-    
+
      <!-- <?php
         $nome = "Aline";
         $usuario = ["Aline", "Naoe", 30];
